788b3c7f648abc6d8dbcbdb284edf00190811257
unknown <ivan.forero-t@labinfo.is.escuelaing.edu.co>
2025-03-27 18:14:28 -0500

    Services

--- src/main/java/com/parcialT2/parcialT2/controller/BuyController.java
+++ src/main/java/com/parcialT2/parcialT2/controller/BuyController.java
@@ -1,5 +1,21 @@
 package com.parcialT2.parcialT2.controller;
 
-public class BuyController {
+import com.parcialT2.parcialT2.service.BuyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v1/buy")
+public class BuyController {
+    @Autowired
+    private BuyService buyService;
+
+    @GetMapping("/all")
+    public ResponseEntity <?> getPaymentDetail(){
+        return ResponseEntity.ok(buyService.getBuyDetails());
+    }
+    
 }

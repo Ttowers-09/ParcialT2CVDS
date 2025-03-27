d4403687f6514806daad48cb97232b75cfd0e25c
unknown <ivan.forero-t@labinfo.is.escuelaing.edu.co>
2025-03-27 13:21:02 -0500

    Scarffolding inicial

---
+++ src/test/java/com/parcialT2/parcialT2/ParcialT2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcialT2.parcialT2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialT2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
